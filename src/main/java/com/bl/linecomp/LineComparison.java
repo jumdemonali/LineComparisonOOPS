5257af2c56f914ac9c3ab4241e51dd4e0abc9dc8
jumdemonali <jumdemonali@gmail.com>
2021-09-01 23:21:22 +0530

    Added: Calculating Length of line

--- src/main/java/com/bl/linecomp/LineComparison.java
+++ src/main/java/com/bl/linecomp/LineComparison.java
@@ -1,7 +1,22 @@
 package com.bl.linecomp;
+import java.util.Scanner;
 
 public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome to Line comparison program!!");
+        variables();
+    }
+    public static void variables() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of x ");
+        int x = sc.nextInt();
+        System.out.print("Enter the value of x1 ");
+        int x1 = sc.nextInt();
+        System.out.print("Enter the value of y ");
+        int y = sc.nextInt();
+        System.out.print("Enter the value of y1 ");
+        int y1 = sc.nextInt();
+        double dist = Math.sqrt((x1 - x) * (x1 - x) + (y1 - y) * (y1 - y));
+        System.out.println("Distance = " + dist);
     }
 }
