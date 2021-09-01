41c84ef9b854d1b9329ab12822a2a827aded96eb
jumdemonali <jumdemonali@gmail.com>
2021-09-01 23:09:45 +0530

    Added: Welcome meessage

---
+++ src/main/java/com/bl/linecomp/LineComparison.java
@@ -0,0 +1,7 @@
+package com.bl.linecomp;
+
+public class LineComparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line comparison program!!");
+    }
+}
